983a0364045bbcd47ce18fec1ef5cf12ee9384ea
ravichandrasriram <ravichandrasriram@gmail.com>
2024-10-24 11:04:02 +0530

    Update utils.py

--- yolov3/lib/utils/utils.py
+++ yolov3/lib/utils/utils.py
@@ -65,7 +65,7 @@ def labels_to_class_weights(labels, nc=80):
 def labels_to_image_weights(labels, nc=80, class_weights=np.ones(80)):
     # Produces image weights based on class mAPs
     n = len(labels)
-    class_counts = np.array([np.bincount(labels[i][:, 0].astype(np.int), minlength=nc) for i in range(n)])
+    class_counts = np.array([np.bincount(labels[i][:, 0].astype(np.int64), minlength=nc) for i in range(n)])
     image_weights = (class_weights.reshape(1, nc) * class_counts).sum(1)
     # index = random.choices(range(n), weights=image_weights, k=1)  # weight image sample
     return image_weights
